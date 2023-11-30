151e9b1275a8ad29b446bc3ac7fd9ca4a764d24b
Per Fagrell <per.fagrell@gmail.com>
2023-11-30 16:10:55 +0100

    Initial scaffolding

---
+++ aoc.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+
+import click
+
+
+@click.command
+@click.option("--all", is_flag=True, default=False)
+def main(all):
+    print("Running AOC")
+    print("===========")
+    directories = sorted(Path(__file__).parent.glob("day-*"))
+    if not all:
+        directories = [directories[-1]]
+
+    for directory in directories:
+        print(directory.stem)
+        print("-" * len(directory.stem))
+        part1_path = directory / "part1.py"
+        part2_path = directory / "part2.py"
+        os.system(f"python {part1_path}")
+        os.system(f"python {part2_path}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ day-01/part2.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from resource import RUSAGE_SELF, getrusage
+from time import perf_counter
+
+from colorama import Back, Fore, Style, init
+
+init()
+input = open(Path(__file__).parent / "input.txt").read().split()
+output = 0
+identifier = Path(__file__).stem
+
+
+def solve(input):
+    return "Not Implemented"
+
+
+if __name__ == "__main__":
+    print(f"{identifier}:", end="", flush=True)
+    start = perf_counter()
+    result = solve(input)
+    elapsed = perf_counter() - start
+    max_rss = getrusage(RUSAGE_SELF)[2] / 1024 / 1024
+
+    if result == output:
+        prefix = "✅ " + Back.GREEN + Fore.WHITE
+    else:
+        prefix = "❌ " + Back.RED + Fore.WHITE
+
+    print(f"\t{prefix}{result}{Style.RESET_ALL}\t{Style.DIM}{elapsed:0.3f}s {max_rss:0.3f} MAX_RSS{Style.RESET_ALL}")
+
+
+def test_part1_example_1():
+    assert True == None
