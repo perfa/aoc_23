7c4096a2f0a3672393267363a5a921aaa3622c34
Per Fagrell <per.fagrell@gmail.com>
2023-12-16 20:36:28 +0100

    Day 16

---
+++ src/day-16/part1.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+from resource import RUSAGE_SELF, getrusage
+from time import perf_counter
+
+from colorama import Style, init
+
+init()
+input = open(Path(__file__).parent / "input.txt").read()
+identifier = Path(__file__).stem
+
+
+def out_of_bounds(x, y, width, height):
+    return x >= width or y >= height or x < 0 or y < 0
+
+
+def solve(input):
+    initial_direction = [1, 0]
+    initial_position = [0, 0]
+    map = input.splitlines()
+    width, height = len(map[0]), len(map)
+
+    beams = [(initial_position, initial_direction)]
+    beam_lines = set()
+    while beams:
+        [x, y], [dir_x, dir_y] = beams.pop(0)
+        if out_of_bounds(x, y, width, height):
+            continue
+        if (x, y, dir_x, dir_y) in beam_lines:
+            continue
+        beam_lines.add((x, y, dir_x, dir_y))
+        match map[y][x]:
+            case ".":
+                beams.append(([x + dir_x, y + dir_y], [dir_x, dir_y]))
+            case "/":
+                match [dir_x, dir_y]:
+                    case 1, 0:
+                        beams.append(([x, y - 1], [0, -1]))
+                    case -1, 0:
+                        beams.append(([x , y + 1], [0, 1]))
+                    case 0, 1:
+                        beams.append(([x - 1, y], [-1, 0]))
+                    case 0, -1:
+                        beams.append(([x + 1, y], [1, 0]))
+            case "\\":
+                match [dir_x, dir_y]:
+                    case 1, 0:
+                        beams.append(([x, y + 1], [0, 1]))
+                    case -1, 0:
+                        beams.append(([x , y - 1], [0, -1]))
+                    case 0, 1:
+                        beams.append(([x + 1, y], [1, 0]))
+                    case 0, -1:
+                        beams.append(([x - 1, y], [-1, 0]))
+            case "|":
+                match [dir_x, dir_y]:
+                    case _, 0:
+                        beams.append(([x , y + 1], [0, 1]))
+                        beams.append(([x , y - 1], [0, -1]))
+                    case 0, _:
+                        beams.append(([x + dir_x, y + dir_y], [dir_x, dir_y]))
+            case "-":
+                match [dir_x, dir_y]:
+                    case 0, _:
+                        beams.append(([x + 1, y], [1, 0]))
+                        beams.append(([x - 1, y], [-1, 0]))
+                    case _, 0:
+                        beams.append(([x + dir_x, y + dir_y], [dir_x, dir_y]))
+    energized = set([(x, y) for x, y, _, _ in beam_lines])
+    return len(energized)
+
+
+if __name__ == "__main__":
+    print(f"{identifier}:", end="", flush=True)
+    start = perf_counter()
+    result = solve(input)
+    elapsed = perf_counter() - start
+    max_rss = getrusage(RUSAGE_SELF)[2] / 1024 / 1024
+
+    result = str(result).ljust(20)
+    print(f"\t{result}{Style.DIM}{elapsed:0.3f}s {max_rss:0.3f} MAX_RSS{Style.RESET_ALL}")
+
+
+def test_part1_example_1():
+    input = r""".|...\....
+|.-.\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\..
+.-.-/..|..
+.|....-|.\
+..//.|...."""
+    assert solve(input) == 46
